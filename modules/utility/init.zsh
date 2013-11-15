75cd37dd188bd741b09a95c91d8d01ae29e41edb
Raine Virta <raine.virta@gmail.com>
2013-11-15 11:35:06 +0200

    alias l: don't show in one column

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -89,7 +89,7 @@ else
   fi
 fi
 
-alias l='ls -1A'         # Lists in one column, hidden files.
+alias l='ls -A'          # Lists in one column, hidden files.
 alias ll='ls -lh'        # Lists human readable sizes.
 alias lr='ll -R'         # Lists human readable sizes, recursively.
 alias la='ll -A'         # Lists human readable sizes, hidden files.
